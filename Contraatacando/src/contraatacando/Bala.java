5d1ca8903211552894a7ad55a4dc7914adddc349
Melissa Treviño <mely.trevic@gmail.com>
2016-02-24 13:13:54 -0600

    Actualización de la clase Bala
    Se hicieron modificaciones en el constructor, se eliminó uno extra, y
    se terminó el método avanza

--- Contraatacando/src/contraatacando/Bala.java
+++ Contraatacando/src/contraatacando/Bala.java
@@ -13,29 +13,49 @@ import java.awt.Image;
  */
 public class Bala extends Base{    
     
-    char cTipo;     // Reconocedor de tipo de bala: 
-                    // a(arriba), i(izquierda), d(derecha).
+    char cTipo;     // Reconocedor de directtion de la bala.
     int iVel;       // Variable para controlar la velocidad de la bala.
     
-    public Bala(int iX, int iY, Image imaImagen) {
+    /**
+     * Bala
+     * 
+     * Método constructor usado para crear el objeto bala
+     * creando el icono a partir de una imagen.
+     * 
+     * @param iX es la <code>posición en x</code> del objeto.
+     * @param iY es la <code>posición en y</code> del objeto
+     * @param imaImagen es la <code>imagen</code> del objeto.
+     * @param cTipo es la <code>dirección</code> de la bala.
+     * @param iVel es la <code>velocidad</code> del objeto
+     */
+    public Bala(int iX, int iY, Image imaImagen, char cTipo, int iVel) {
         super(iX, iY, imaImagen);
+        this.cTipo = cTipo;
+        this.iVel = iVel;
     }
     
-    public Bala(int iX, int iY, Image imaImage, char cTipo, int iVel) {
-        super(iX, iY, imaImage);
-        cTipo = 'a';
-        iVel = 3;
-    }
-    
+    /**
+     * avanza
+     * 
+     * Método que actualiza la posición de la bala, según la dirección en
+     * la que vaya.
+     */
     public void avanza(){
         switch(cTipo){
+                // Caso en que la bala vaya hacia arriba.
+                case ' ': {
+                    this.setY(this.getY() - this.iVel);
+                    break;
+                }
+                // Caso en que la bala vaya hacia la izquierda.
                 case 'a': {
+                    this.setY(this.getY() - this.iVel);
                     break;
                 }
-                case 'b': {
-                    break;
-                }
-                case 'c': {
+                // Caso en que la bala vaya hacia la derecha.
+                case 's': {
+                    this.setY(this.getY() - this.iVel);
+                    this.setX(this.getX() - this.iVel);
                     break;
                 }
         }
