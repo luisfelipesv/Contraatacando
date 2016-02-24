86e1fe180e3bd18047f2975fe4067f5d779a5e76
Melissa Treviño <mely.trevic@gmail.com>
2016-02-24 12:58:32 -0600

    Contractor y avanza (Bala)
    Desarrollé el constructor de la clase Bala y la función de avanza

--- Contraatacando/src/contraatacando/Bala.java
+++ Contraatacando/src/contraatacando/Bala.java
@@ -5,10 +5,39 @@
  */
 package contraatacando;
 
+import java.awt.Image;
+
 /**
  *
  * @author luisfelipesv
  */
-public class Bala {
+public class Bala extends Base{    
     
+    char cTipo;     // Reconocedor de tipo de bala: 
+                    // a(arriba), i(izquierda), d(derecha).
+    int iVel;       // Variable para controlar la velocidad de la bala.
+    
+    public Bala(int iX, int iY, Image imaImagen) {
+        super(iX, iY, imaImagen);
+    }
+    
+    public Bala(int iX, int iY, Image imaImage, char cTipo, int iVel) {
+        super(iX, iY, imaImage);
+        cTipo = 'a';
+        iVel = 3;
+    }
+    
+    public void avanza(){
+        switch(cTipo){
+                case 'a': {
+                    break;
+                }
+                case 'b': {
+                    break;
+                }
+                case 'c': {
+                    break;
+                }
+        }
+    }
 }
